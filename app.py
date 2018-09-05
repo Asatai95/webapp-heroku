f9322ed3b84fa67845688a07578744907b112672
Taishi Asato <defense433@gmail.com>
2018-09-05 21:03:49 +0900

    test

--- app.py
+++ app.py
@@ -182,8 +182,8 @@ def text_db():
     con = db.cursor()
     print('???')
 
-    sql = 'insert into test(test) values(%s)'
-    text = con.execute(sql, (form))
+    sql = 'insert into test(test) values(?)'
+    text = con.execute(sql, [form])
     db.commit()
     print(text)
 

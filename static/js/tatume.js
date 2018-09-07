f01468cbcf50f4d13921627033731aaad1a215c2
Taishi Asato <defense433@gmail.com>
2018-09-08 00:33:42 +0900

    test

--- static/js/tatume.js
+++ static/js/tatume.js
@@ -1,3 +1,26 @@
 $(function(){
   $('html').fadeIn(800);
 });
+
+$(function () {
+  $('div.header').each(function () {
+    var $win = $(window),
+        $winH = $win.height(),
+        $connect = $(this),
+        position = $connect.offset().top,
+        current = 0,
+        scroll;
+    $win.on('load scroll', function () {
+      scroll = $win.scrollTop();
+      current = (1 - (position - scroll) / $winH) * 1 * 100;
+      if (current > 99.9) {
+        current = 100;
+      }
+      if (scroll > position - $winH) {
+        $('header_sub').css('position', 'fixed');
+        $('header_sub').css('background-color', 'rgba(255, 255, 255, 0.7)');
+        $('header_sub').css('z-index', '5');
+      }
+    });
+  });
+});

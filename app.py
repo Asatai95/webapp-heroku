e6122ecd03a848c7110a362c6e277c8205005527
Taishi Asato <defense433@gmail.com>
2018-09-08 17:47:27 +0900

    更新

--- app.py
+++ app.py
@@ -71,10 +71,10 @@ def img():
     db = MySQLdb.connect(user='b292b90b1818e0', passwd='4346c8fc', host='us-cdbr-iron-east-01.cleardb.net', db='heroku_ae66112c0cf1b10', charset='utf8')
     con = db.cursor()
 
-    sql = 'select img from img'
-    img = con.execute(sql)
+    sql = 'select id, img from img'
+    image = con.execute(sql)
     db.commit()
-    print(img)
+    print(image)
 
     result = con.fetchall()
     result = result[0][0]

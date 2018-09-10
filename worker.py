28e90a20d37852494e67505573cd8f5322312289
Taishi Asato <defense433@gmail.com>
2018-09-10 23:19:53 +0900

    更新

--- worker.py
+++ worker.py
@@ -3,13 +3,9 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
-listen = '/oauth2callback'
-
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:8080')
 
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+    main()

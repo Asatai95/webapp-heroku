30dac18b309b04dd83f5954cab2fc8d9377ecde1
Taishi Asato <defense433@gmail.com>
2018-09-10 23:28:00 +0900

    更新

--- worker.py
+++ worker.py
@@ -3,9 +3,13 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
+listen = 'http://localhost:8080'
+
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:8080')
 
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    
+    with Connection(conn):
+        worker = Worker(listen)
+        worker.work()

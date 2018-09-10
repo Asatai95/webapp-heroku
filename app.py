0198889f7b95d061b82ab38ff8decf9e3de7cdc0
Taishi Asato <defense433@gmail.com>
2018-09-10 19:02:30 +0900

    更新

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email import message
 from email import charset
-from bs4 import BeautifulSoup
+import BeautifulSoup
 import email
 import base64
 import smtplib
